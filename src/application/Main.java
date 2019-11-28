08d4cd3b705b43047beaaad5b6abedf097c15776
Eduardoisaac23 <edugmendes.facisabh@gmail.com>
2019-11-28 15:48:56 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,11 +17,15 @@ public class Main extends Application {
 		try {                                                        //este e o caminho da view
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			//depois chamamos um loader.load(); e carrega a View
-			Parent parent = loader.load();
+			ScrollPane scrollPane = loader.load();
+			//Altura da scroll
+			scrollPane.setFitToHeight(true);
+			//largura da scroll
+			scrollPane.setFitToWidth(true);
 			// criar um objeto do tipo Scene  que terá mainScene como sena principal
 			// e istânciado esta sena já passando como argumento o bojeto principal da minha View
 			//No caso e o AnchorPane vasio
-			Scene mainScene = new Scene(parent);//resumindo criou a sena
+			Scene mainScene = new Scene(scrollPane);//resumindo criou a sena
 			// primaryStage palco da minha sena que etá estanciado no meu metodo void 
 			//será setado como minha sena principal
 			primaryStage.setScene(mainScene);
