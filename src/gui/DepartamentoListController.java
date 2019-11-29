01348ae6dd271e52e1ec1c9a89986fdded88238f
Eduardoisaac23 <edugmendes.facisabh@gmail.com>
2019-11-29 16:59:58 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartamentoListController.java
+++ src/gui/DepartamentoListController.java
@@ -1,18 +1,26 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Services.DepartamentoService;
 import model.entities.Departmento;
@@ -43,8 +51,9 @@ public class DepartamentoListController implements Initializable {
 	
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = gui.util.Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentoForm.fxml", parentStage);
 	}
 	
 	//metodo para injetar dependência por outro lugar
@@ -84,6 +93,26 @@ public class DepartamentoListController implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartamento.setItems(obsList);
 		
+	}
+	
+	//finção para carregar a janela do formulário de um novo departamento
+	private void createDialogForm(String absoluteName, Stage parenStage) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com o dado do departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parenStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}catch(IOException e ) {
+			Alerts.showAlert("IO Exception", "Erroao carregar a view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
